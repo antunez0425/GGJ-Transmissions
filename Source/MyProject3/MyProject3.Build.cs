8d69d5e2351b64e711d2079711b89f922a46712d
arcticfaded <jbelt021@fiu.edu>
2018-01-27 12:51:28 -0500

    for version 4.18.3

--- Source/MyProject3/MyProject3.Build.cs
+++ Source/MyProject3/MyProject3.Build.cs
@@ -4,8 +4,13 @@ using UnrealBuildTool;
 
 public class MyProject3 : ModuleRules
 {
-	public MyProject3(TargetInfo Target)
+	public MyProject3(ReadOnlyTargetRules Target) : base (Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		MinFilesUsingPrecompiledHeaderOverride = 1;
+		bFasterWithoutUnity = true;
+
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "AIModule", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {"Slate", "SlateCore"});
 	}
 }
